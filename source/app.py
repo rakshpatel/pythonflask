10b6483be3f0274b4f9377d36a6a75ae2e6cce96
Rakhit Patel <raksh.patel@gmail.com>
2022-12-04 11:32:27 +0530

    Adds more logic
    Adds logic to load data from sample csvs
    Adds logic to read data for Ships and Shipspositions
    Adds required API endpoints

--- source/app.py
+++ source/app.py
@@ -1,13 +1,42 @@
 from flask import Flask
-
+from flask_restful import Resource, Api, reqparse
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key="polestartships"
+api = Api(app)
+DATABASE_PATH="shipsdata.db"
+conn = sqlite3.connect(DATABASE_PATH)
 
+class Ships(Resource):
+    def get(self):
+        conn = sqlite3.connect(DATABASE_PATH)
+        query = "SELECT imo, shipname FROM SHIPS "
+        curs = conn.cursor()
+        shipdata = []
+        for ships in curs.execute(query):
+            shipdata.append({"imo": ships[0], "name": ships[1]})
+        conn.close()
+        return {"ship": shipdata}
+
+
+class ShipsPosition(Resource):
+    def get(self, imo):
+        conn = sqlite3.connect(DATABASE_PATH)
+        query = "SELECT imo, datetime, latitude, longitude FROM SHIPSPOSITION WHERE imo={imo} ORDER BY 2 ".format(imo=imo)
+        curs = conn.cursor()
+        shipspositions = []
+        for shipsposition in curs.execute(query):
+            shipspositions.append({"datetime": shipsposition[1], "latitude": shipsposition[2], "longitude": shipsposition[3]})
+        conn.close()
+        return {"ship": {"imo": imo, "positions": shipspositions}}
 
 @app.route("/")
 def index():
     return "Hello World"
 
+api.add_resource(Ships, "/api/ships/")
+api.add_resource(ShipsPosition, "/api/positions/<int:imo>")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ source/sampledata/loadData.py
@@ -0,0 +1,22 @@
+import sqlite3
+import pandas as pd
+
+#DATABASE_PATH="../shipsdata.db"
+DATABASE_PATH="tempdb.db"
+conn = sqlite3.connect(DATABASE_PATH)
+
+
+ship_data = pd.read_csv(".\ships.csv")
+ship_data.to_sql("ships", conn, if_exists='replace')
+
+
+shipsposition_data = pd.read_csv(".\shipsposition.csv")
+shipsposition_data.to_sql("shipsposition", conn, if_exists='replace')
+
+cur = conn.cursor()
+
+for row in cur.execute("SELECT * FROM ships"):
+    print(row)
+
+for row in cur.execute("SELECT * FROM shipsposition"):
+    print(row)
